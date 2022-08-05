9df3693e67169cc4995082a67a03fb76bad2d44b
cool-eth <icpigci@gmail.com>
2022-08-05 12:58:31 -0400

    velodrom LPAggregator & swappers

---
+++ contracts/airUSDSwappers/optimism/VelodromeLPSwapper.sol
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.9;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+import "../../interfaces/ISwapper.sol";
+import "../../interfaces/ISwapperImpl.sol";
+import "../../external/velodrome/IVelodromeRouter.sol";
+import "../../external/velodrome/IVelodromePair.sol";
+
+contract VelodromeLPSwapper is ISwapperImpl {
+    using SafeERC20 for IERC20;
+
+    address public immutable swapper;
+    address public immutable override tokenIn; // velodrome pair
+    address public immutable override tokenOut;
+    address public immutable velodromeRouter;
+    address public immutable token0;
+    address public immutable token1;
+    bool public immutable isStable;
+
+    constructor(
+        address _swapper,
+        address _velodromeRouter,
+        address _tokenIn,
+        address _tokenOut
+    ) {
+        swapper = _swapper;
+        velodromeRouter = _velodromeRouter;
+        tokenIn = _tokenIn;
+        tokenOut = _tokenOut;
+        token0 = IVelodromePair(tokenIn).token0();
+        token1 = IVelodromePair(tokenIn).token1();
+        isStable = IVelodromePair(tokenIn).stable();
+
+        IERC20(tokenIn).safeApprove(_velodromeRouter, type(uint256).max);
+        IERC20(token0).safeApprove(_velodromeRouter, type(uint256).max);
+        IERC20(token1).safeApprove(_velodromeRouter, type(uint256).max);
+    }
+
+    function swap(uint256 _amountIn, address _to)
+        external
+        override
+        returns (uint256 amountOut)
+    {
+        IERC20(tokenIn).safeTransferFrom(msg.sender, address(this), _amountIn);
+
+        IVelodromeRouter(velodromeRouter).removeLiquidity(
+            token0,
+            token1,
+            isStable,
+            _amountIn,
+            0,
+            0,
+            address(this),
+            block.timestamp
+        );
+
+        uint256 token0Balance = IERC20(token0).balanceOf(address(this));
+        if (token0Balance > 0) {
+            amountOut += ISwapper(swapper).swap(
+                token0,
+                tokenOut,
+                token0Balance,
+                _to
+            );
+        }
+
+        uint256 token1Balance = IERC20(token1).balanceOf(address(this));
+        if (token1Balance > 0) {
+            amountOut += ISwapper(swapper).swap(
+                token1,
+                tokenOut,
+                token1Balance,
+                _to
+            );
+        }
+    }
+}
---
+++ contracts/external/velodrome/IVelodromeRouter.sol
@@ -0,0 +1,164 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity 0.8.9;
+pragma experimental "ABIEncoderV2";
+
+interface IVelodromeRouter {
+    struct route {
+        address from;
+        address to;
+        bool stable;
+    }
+
+    function factory() external pure returns (address);
+
+    function weth() external pure returns (address);
+
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        bool stable,
+        uint256 amountADesired,
+        uint256 amountBDesired,
+        uint256 amountAMin,
+        uint256 amountBMin,
+        address to,
+        uint256 deadline
+    )
+        external
+        returns (
+            uint256 amountA,
+            uint256 amountB,
+            uint256 liquidity
+        );
+
+    function addLiquidityETH(
+        address token,
+        bool stable,
+        uint256 amountTokenDesired,
+        uint256 amountTokenMin,
+        uint256 amountETHMin,
+        address to,
+        uint256 deadline
+    )
+        external
+        payable
+        returns (
+            uint256 amountToken,
+            uint256 amountETH,
+            uint256 liquidity
+        );
+
+    function removeLiquidity(
+        address tokenA,
+        address tokenB,
+        bool stable,
+        uint256 liquidity,
+        uint256 amountAMin,
+        uint256 amountBMin,
+        address to,
+        uint256 deadline
+    ) external returns (uint256 amountA, uint256 amountB);
+
+    function removeLiquidityETH(
+        address token,
+        bool stable,
+        uint256 liquidity,
+        uint256 amountTokenMin,
+        uint256 amountETHMin,
+        address to,
+        uint256 deadline
+    ) external returns (uint256 amountToken, uint256 amountETH);
+
+    function removeLiquidityWithPermit(
+        address tokenA,
+        address tokenB,
+        bool stable,
+        uint256 liquidity,
+        uint256 amountAMin,
+        uint256 amountBMin,
+        address to,
+        uint256 deadline,
+        bool approveMax,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external returns (uint256 amountA, uint256 amountB);
+
+    function removeLiquidityETHWithPermit(
+        address token,
+        bool stable,
+        uint256 liquidity,
+        uint256 amountTokenMin,
+        uint256 amountETHMin,
+        address to,
+        uint256 deadline,
+        bool approveMax,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external returns (uint256 amountToken, uint256 amountETH);
+
+    function swapExactTokensForTokensSimple(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address tokenFrom,
+        address tokenTo,
+        bool stable,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
+
+    function swapExactTokensForTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        route[] calldata routes,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
+
+    function swapExactETHForTokens(
+        uint256 amountOutMin,
+        route[] calldata routes,
+        address to,
+        uint256 deadline
+    ) external payable returns (uint256[] memory amounts);
+
+    function getReserves(
+        address tokenA,
+        address tokenB,
+        bool stable
+    ) external view returns (uint256 reserveA, uint256 reserveB);
+
+    function getAmountOut(
+        uint256 amountIn,
+        address tokenIn,
+        address tokenOut
+    ) external view returns (uint256 amount, bool stable);
+
+    function getAmountsOut(uint256 amountIn, route[] memory routes)
+        external
+        view
+        returns (uint256[] memory amounts);
+
+    function quoteAddLiquidity(
+        address tokenA,
+        address tokenB,
+        bool stable,
+        uint256 amountADesired,
+        uint256 amountBDesired
+    )
+        external
+        view
+        returns (
+            uint256 amountA,
+            uint256 amountB,
+            uint256 liquidity
+        );
+
+    function quoteRemoveLiquidity(
+        address tokenA,
+        address tokenB,
+        bool stable,
+        uint256 liquidity
+    ) external view returns (uint256 amountA, uint256 amountB);
+}
